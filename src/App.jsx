296700f4bed7d37a56735206ce6822b961234e99
Eric Huang <huangeric.j@gmail.com>
2022-04-29 12:04:40 -0700

    Import resume directly from google drive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter as Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './components/Home';
@@ -7,17 +7,30 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-const App = () => (
-  <div id='app'>
-    <Route path='/#home'><Home /></Route>
-    <main>
-      <Navigation />
-      <Route path='/#about'><About /></Route>
-      <Route path='/#skills'><Skills /></Route>
-      <Route path='/#projects'><Projects /></Route>
-      <Route path='/#contact'><Contact /></Route>
-    </main>
-  </div>
-);
+const App = () => {
+  const [url, setUrl] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
 
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('https://docs.google.com/document/d/1irOkn5n4OTZkBu2omhrv-Bm-5c7NoL6if-AU9UtxWjg/export?format=pdf');
+      const blob = await res.blob();
+      setUrl(URL.createObjectURL(blob));
+      setIsLoaded(true);
+    })();
+  }, []);
+
+  return isLoaded && (
+    <div id='app'>
+      <Route path='/#home'><Home /></Route>
+      <main>
+        <Navigation />
+        <Route path='/#about'><About resume={url} /></Route>
+        <Route path='/#skills'><Skills /></Route>
+        <Route path='/#projects'><Projects /></Route>
+        <Route path='/#contact'><Contact /></Route>
+      </main>
+    </div>
+  );
+};
 export default App;
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import profile from '../../assets/profile.png';
-import resume from '../../assets/resume.pdf';
 import './About.scss';
 
 const about = 'I am a passionate and creative Software Engineer who loves putting ideas together into a working application. I have experience building dynamic web applications in JavaScript and Python using Node.js, Express.js, Flask, React/Redux, PostgreSQL, and MongoDB, and creating Discord Bots using Discord.js. Currently, I am planning/working on creating an application that helps manage boss drops for one of my favorite video games, as well as creating my very own text-based RPG that will live inside a Discord Bot.';
 
-const About = () => (
+const About = ({ resume }) => (
   <section id='about'>
     <h2 className='heading'>About Me</h2>
     <div className='content'>
